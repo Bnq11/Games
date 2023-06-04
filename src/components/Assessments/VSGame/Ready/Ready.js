e433da98744e8e91d76a6778de8b27ef467c6db4
Bashair Alqahtani <96373687+Bnq11@users.noreply.github.com>
2023-06-04 03:36:51 +0300

    Ready Cards

--- src/components/Assessments/VSGame/Ready/Ready.js
+++ src/components/Assessments/VSGame/Ready/Ready.js
@@ -14,7 +14,28 @@ function Ready(){
         { id: 3, img: '/img/3yb.png',  stat:"active"},
         { id: 4, img: '/img/4bc.png',  stat:"active"},
     ]);
- 
+    const [isStarted , setisStarted] = useState(true);
+    const [isStarted1 , setisStarted1] = useState(false);
+    const [isStarted2 , setisStarted2] = useState(false);
+    const [start , setstart] = useState(false);
+
+    function onClick(id){
+           if (id ===1) {
+            setisStarted(false);
+            setisStarted1(true);}
+            else {
+                if(id === 2){
+                setisStarted1(false);
+            setisStarted2(true);
+                } 
+                else{
+                    if(id === 2){
+                    setisStarted2(false);
+                    setstart(true);  
+                    } 
+                }
+            }
+    }
     return (
         
         <div className="Ready">
@@ -22,18 +43,31 @@ function Ready(){
             <div className="tips">
                 <h5 className='tips-header shadow'> 
                     .ستحصل على مجموعتين من البطاقات ، كل واحدة تحتوي على 64 بطاقة <br/>
-                    .سيتم عرض بطاقة واحدة في كل مرة وسيُطلب منك فرزها، يمكنك فرز البطاقة من خلال النقر فوق إحدى البطاقات الأربع التي تراها أدناه <br/>
-                    .بحسب اللون،او الشكل، او العدد. عندما تفرز البطاقة بشكل صحيح ، ستظهر كلمة "صحيح" على الشاشة <br/>
-                    ."ولكن إذا تم فرز البطاقة بشكل خاطئ، فستظهر كلمة "خطأ <br/>
-                    .الهدف هو الحصول على أكبر عدد ممكن من البطاقات الصحيحة 
+                سيتم عرض بطاقة واحدة في كل مرة وسيُطلب منك فرزها 
+                  
                 </h5>
+            
             </div>
-            <div className="container">
+             <div className="tips">
+                <h5 className='tips-header shadow'> 
+                يمكنك فرز البطاقة من خلال النقر فوق إحدى البطاقات الأربع التي تراها  <br/>
+                <div className="container">
                 { cards.map((card, index) => (
                     <Card key={index} item={card} id={index}/>
                     ))
                 }
              </div>
+                    بحسب اللون،او الشكل، او العدد
+                </h5>
+            </div>
+             <div className="tips">
+                <h5 className='tips-header shadow'> 
+                عندما تفرز البطاقة بشكل صحيح ، ستظهر كلمة "صحيح" على الشاشة <br/>
+                    ."ولكن إذا تم فرز البطاقة بشكل خاطئ، فستظهر كلمة "خطأ <br/>
+                    .الهدف هو الحصول على أكبر عدد ممكن من البطاقات الصحيحة 
+                </h5>
+            </div>
+           
 
              <div className='button-justifyer'>
             <Link to="/Cards" className="btn">ابدأ</Link>
