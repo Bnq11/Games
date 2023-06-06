bf7001f95ecd6488a0862dfc931452c6e7df3fb4
Bashair Alqahtani <96373687+Bnq11@users.noreply.github.com>
2023-06-06 21:59:08 +0300

    cards

--- src/components/Assessments/VSGame/Ready/Ready.js
+++ src/components/Assessments/VSGame/Ready/Ready.js
@@ -8,56 +8,80 @@ import Background from '../../../Background/Background';
 import Videopopup from '../../../Videopopup';
 import demo from '../../BART/assets/BRAT.mp4';
 
-// import Card from '../Card/Card';
+import Card from '../Card/Card';
 
 function Ready(){
     const [openPopup, setOpenPopup] = useState(false);
     function handleOpen(){
         setOpenPopup(true)
     }
+    const[cardin , setcardin]= useState(true);
     const [card1,setCard1] = useState(false)
     const [card2,setCard2] = useState(false)
     const [card3,setCard3] = useState(false)
     function handleClose(){
         setOpenPopup(false)
     } 
-    // const [cards] = useState([
-    //     { id: 1, img: '/img/1rt.png', stat:"active" },
-    //     { id: 2, img: '/img/2gs.png',  stat:"active"},
-    //     { id: 3, img: '/img/3yb.png',  stat:"active"},
-    //     { id: 4, img: '/img/4bc.png',  stat:"active"},
-    // ]);
- 
+    const [cards] = useState([
+        { id: 1, img: '/img/1rt.png', stat:"active" },
+        { id: 2, img: '/img/2gs.png',  stat:"active"},
+        { id: 3, img: '/img/3yb.png',  stat:"active"},
+        { id: 4, img: '/img/4bc.png',  stat:"active"},
+    ]);
+
     return (
         
-        <div className="Ready">
-            <h1 className='title'> مهمة فرز البطاقات </h1>
-            <div className="tips">
-                <div className='headersandbutton my-1'>
+        <div className="">
+
+            <h1 className='title'> مهمة  فرز البطاقات </h1> 
+
+            <div className="">
+            {cardin &&    <div className='headersandbutton my-1'>
                 <h3 className='tips-header'> 
-                    .ستحصل على مجموعتين من البطاقات ، كل واحدة تحتوي على 64 بطاقة
-                </h3>
-                <button className='btnt m-0' onClick={()=>setCard1(true)}>التالي</button>
-                </div>
+                في هذه اللعبة، مهمتك بسيطة!
+
+خلال هذه اللعبة سنعرض عليك بطاقة وعليك تصنيفها الى أحد مجموعات البطاقات المعروضة.
+ </h3>
+ <div className="containerinf">
+                { cards.map((card, index) => (
+                    <Card key={index} item={card} id={index}/>
+                    ))
+                }
+                
+             </div>
+                <button className='btntrvs m-0' onClick={()=>{setCard1(true);
+               setcardin(() => false);}}>
+                    {'< '} التالي</button>
+                </div>}
                 {card1 &&
                 <div className='headersandbutton my-1'>
                 <h3 className='tips-header'> 
-                    .ستحصل على مجموعتين من البطاقات ، كل واحدة تحتوي على 64 بطاقة <br/>
-                    .سيتم عرض بطاقة واحدة في كل مرة وسيُطلب منك فرزها، يمكنك فرز البطاقة من خلال النقر فوق إحدى البطاقات الأربع التي تراها أدناه <br/>
+                  
+اذا فرزت البطاقة بشكل صحيح، سيظهر كلمة "صحيح". اذا فرزتها بشكل خاطئ سيظهر "خاطئ"
+عليك الحصور على أكبر عدد ممكن من الفرز الصحيح
                 </h3>
-                <button className='btnt m-0' onClick={()=>setCard2(true)}>التالي</button>
+                {/* <button className='btntrvs m-0' onClick={()=>{setCard2(true);setCard1(false)}}>  {'< '} التالي</button> */}
+                <div className='button-justifyer'>
+                <Link to="/Cards" className="btn">ابدأ</Link>
+                <button className="btnt2" onClick={handleOpen}>فيديو توضيحى</button>
+            </div>
                 </div>
                 }
-                { card2 &&
+                {/* { card2 &&
                 <div className='headersandbutton my-1'>
                 <h3 className='tips-header'> 
                     .بحسب اللون،او الشكل، او العدد. عندما تفرز البطاقة بشكل صحيح ، ستظهر كلمة "صحيح" على الشاشة <br/>
                     ."ولكن إذا تم فرز البطاقة بشكل خاطئ، فستظهر كلمة "خطأ <br/>
                     .الهدف هو الحصول على أكبر عدد ممكن من البطاقات الصحيحة 
                 </h3>
-                <button className='btnt m-0' onClick={()=>setCard3(true)}>التالي</button>
-                </div>
-                }
+                <div className='button-justifyer'>
+                <Link to="/Cards" className="btn">ابدأ</Link>
+                <button className="btnt2" onClick={handleOpen}>فيديو توضيحى</button>
+            </div> */}
+                {/* <button className='btntrvs m-0' onClick={()=>{setCard3(true);
+                setCard2(false)}}>  {'< '} التالي</button> */}
+                {/* </div> */}
+                {/* } */}
             </div>
 
             {/* <div className="container">
@@ -79,12 +103,12 @@ function Ready(){
                 handleClose={handleClose}
             /> 
 
-            { card3 &&
+            {/* { card3 &&
             <div className='button-justifyer'>
                 <Link to="/Cards" className="btn">ابدأ</Link>
                 <button className="btnt2" onClick={handleOpen}>فيديو توضيحى</button>
             </div>
-            }
+            } */}
 
             <Background/> 
         </div>
